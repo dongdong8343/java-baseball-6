a48f9ddcee5d04c68065b422f919cd4e5f31684b
dongdong8343 <fkdlem8343@naver.com>
2025-01-01 22:43:06 +0900

    플레이어 수 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ import camp.nextstep.edu.missionutils.*;
 public class Application {
     static int[] arr = new int[10];
     static String answer = "";
-    public static void GetNum(){
+    public static void GetAnswer(){
         while(answer.length() < 3){
             int num = Randoms.pickNumberInRange(1, 9);
             if(arr[num] == 1) continue;
@@ -11,8 +11,23 @@ public class Application {
             answer += num;
         }
     }
+    // 플레이어 수 입력 -> 잘못 입력하면 예외 처리
+    public static int InputNum(){
+        int num;
+        System.out.print("숫자를 입력해주세요 : ");
+        try{
+            num = Integer.parseInt(Console.readLine());
+            if(String.valueOf(num).length() != 3) {
+                IllegalArgumentException e = new IllegalArgumentException();
+                throw e;
+            }
+        }catch (IllegalStateException e){
+            return -1;
+        }
+        return num;
+    }
     public static void main(String[] args) {
-        GetNum();
-
+        GetAnswer();
+        System.out.println(InputNum());
     }
 }
